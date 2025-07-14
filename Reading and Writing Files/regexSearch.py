459e76b4b6cf0d45f7ec36b55e0f42a1a908b887
SARAH RUDSTON <sarah.rudston@gmail.com>
2025-07-14 08:49:30 +0100

    start of regex search practice project

---
+++ Reading and Writing Files/regexSearch.py
@@ -0,0 +1,6 @@
+# TODO: Open all txt files in folder 
+# TODO: Read contents to variable
+# TODO: Search for regex expression
+# TODO: User input to supply expression
+# TODO: Print results to screen
+
