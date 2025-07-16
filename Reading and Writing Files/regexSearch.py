e07710e5fa85f7cb696cab51622c864824db2b45
SARAH RUDSTON <sarah.rudston@gmail.com>
2025-07-16 08:22:42 +0100

    finishing regex search practice

--- Reading and Writing Files/regexSearch.py
+++ Reading and Writing Files/regexSearch.py
@@ -1,9 +1,3 @@
-# TODO: Open all txt files in folder 
-# TODO: Read contents to variable
-# TODO: Search for regex expression
-# TODO: User input to supply expression
-# TODO: Print results to screen
-
 import re
 import os
 
@@ -16,7 +10,12 @@ for file in os.listdir(cwd):
 
 print("Enter regex pattern to match:")
 user_input = input()
-user_regex = re.compile(user_input)
+
+try:
+    user_regex = re.compile(user_input)
+except re.error:
+    print("Invalid regex pattern.")
+    exit(1)
 
 found = 0
 
@@ -24,12 +23,10 @@ for file in files:
     with open(file, encoding='utf-8') as f:
         text = f.read()
         result = user_regex.search(text)
-        if result == None:
-            break
-        elif result.group() == user_input:
+        if result:
             print("Found matching content at file: " + (os.path.abspath(file)))
+            print(result)
             found += 1
-            break
 
 if found == 0:
     print("Content not found.")
