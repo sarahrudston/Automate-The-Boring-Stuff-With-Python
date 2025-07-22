439c00783b4a1150edf017df074346cf38dc75c9
SARAH RUDSTON <sarah.rudston@gmail.com>
2025-07-22 08:51:03 +0100

    finishing practice exercise

--- Organizing Files/renumberingFiles.py
+++ Organizing Files/renumberingFiles.py
@@ -2,12 +2,30 @@ import os, shutil
 from pathlib import Path
 import re
 
-#TODO: Get all files with prefix
+def getFilesWithPrefix(folderPath, prefix):
 
-prefix = 'spam'
-folderPath = '.' 
+    fileRegex = re.compile(prefix + '(\\d+)(.\\w+)')
+    fileList = sorted( [file for file in os.listdir(folderPath) if fileRegex.match(file)])
+    return fileList
 
-fileRegex = re.compile(prefix + '(\d+)(.\w+)')
-fileList = sorted( [file for file in os.listdir(folderPath) if fileRegex.match(file)])
+def fillGaps(folderPath, prefix):
+    fileList = getFilesWithPrefix(folderPath, prefix)
+    fileRegex = re.compile(prefix + '(\\d+)(.\\w+)')
 
-start = int(fileRegex.search(fileList[0]).group[1]) #
+    start = int(fileRegex.search(fileList[0]).group(1)) # Files sorted in ascending order
+    count = start
+    maxLength = len(fileRegex.search(fileList[-1]).group(1))
+
+    for file in fileList:
+        mo = fileRegex.search(file)
+        fileNum = int(mo.group(1))
+
+        if fileNum != count:
+            newFileName = prefix + '0' * (maxLength - len(str(count))) + str(count) + mo.group(2)
+            oldPath = os.path.join(folderPath, file)
+            newPath = os.path.join(folderPath, newFileName)
+            shutil.move(oldPath, newPath)
+    
+        count += 1
+
+fillGaps('.', 'spam')
